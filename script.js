705b033e51ae8a017d23bea0f04de62f4200dc75
Arin <arin1998@gmail.com>
2023-01-29 19:51:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const section4=document.querySelector('.section-4');
+
+const section4Left1=document.querySelector('.section-4-left-1');
+const section4Left2=document.querySelector('.section-4-left-2');
+const section4Left3=document.querySelector('.section-4-left-3');
+const section4Left4=document.querySelector('.section-4-left-4');
+
+const section4RightImg=document.querySelector('.section-4-right-img');
+
+const section4RightImage2="https://web-images.credcdn.in/_next/assets/images/home-page/features/neopop-fold1.png";
+const section4RightImage1="https://web-images.credcdn.in/_next/assets/images/home-page/features/neopop-fold2.png";
+const section4RightImage3="https://web-images.credcdn.in/_next/assets/images/home-page/features/neopop-fold3.png";
+const section4RightImage4="https://web-images.credcdn.in/_next/assets/images/home-page/features/neopop-fold4.png";
+
+//add sccroll event listener
+
+window.addEventListener('scroll',()=>{
+    const scrollPosition=window.scrollY;
+    const section4OffsetTop=section4.offsetTop;
+    const section4Height=section4.clientHeight;
+})
